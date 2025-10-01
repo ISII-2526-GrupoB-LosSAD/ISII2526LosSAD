0c5ba95933ad122bebc2acfd7be24157f05eba10
Antonsqd <antoniosqdhellin@gmail.com>
2025-10-01 10:35:32 +0200

    Creación de la clase Repair

---
+++ src/AppForSEII2526.API/Models/Repair.cs
@@ -0,0 +1,12 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Repair
+    {
+        public float Cost { get; set; }
+        public string Description { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Scaledid { get; set; }
+    }
+}
