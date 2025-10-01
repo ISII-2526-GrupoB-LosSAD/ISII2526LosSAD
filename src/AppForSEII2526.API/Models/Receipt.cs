d9bb15e8e8da4c6283456622dc678857032cd2b4
Antonsqd <antoniosqdhellin@gmail.com>
2025-10-01 11:08:56 +0200

    Crear clase Receipt

---
+++ src/AppForSEII2526.API/Models/Receipt.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+        public enum PaymentMethodTypes
+        {
+            //Metodos de pago
+        }
+        public DateTime ReceiptDate { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
