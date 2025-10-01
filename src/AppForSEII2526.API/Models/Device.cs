7ec1b17711f620327a397364e5addb3d724d4aec
tierrasecadaniel1-bit <tierrasecadaniel1@gmail.com>
2025-10-01 11:28:16 +0200

    Crear la clase Device

---
+++ src/AppForSEII2526.API/Models/Device.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Device
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double priceForPurchase { get; set; }
+        public double priceForRent { get; set; }
+
+        public string Quality { get; set; }
+
+        public int quauntityForPurchase { get; set; }
+
+        public int quauntityForRent { get; set; }
+
+        public int Year { get; set; }
+    }
+}
