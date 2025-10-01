08f28b44e547e4e6cb0826625cb3b35ccac6c85f
Antonsqd <antoniosqdhellin@gmail.com>
2025-10-01 10:24:51 +0200

    Creación de la clase scale.

--- src/AppForSEII2526.API/Models/Scale.cs
+++ src/AppForSEII2526.API/Models/Scale.cs
@@ -1,7 +1,7 @@
 ﻿namespace AppForSEII2526.API.Models
 {
     public class Scale
-    {
+    {//Creacion de la clase Scale con id como llave primaria y name como string
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
