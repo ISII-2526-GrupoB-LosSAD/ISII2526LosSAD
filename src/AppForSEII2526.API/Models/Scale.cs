6d51da9811165f548efc7a37d92a9786d2b0de6b
Antonio Sánchez-Quintanar Díaz-Hellín <Antonio.Sanchez38@alu.uclm.es>
2025-10-01 10:28:33 +0200

    10 tarea 1 crear clase scale (#16)
    * Creación de la clase scale.
    
    ---------
    
    Co-authored-by: Antonsqd <antoniosqdhellin@gmail.com>

---
+++ src/AppForSEII2526.API/Models/Scale.cs
@@ -0,0 +1,9 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Scale
+    {//Creacion de la clase Scale con id como llave primaria y name como string
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
