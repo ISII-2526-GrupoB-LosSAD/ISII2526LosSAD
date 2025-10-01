9800f65d6322dc71ebbc2b42b83e4830eb852456
Antonsqd <antoniosqdhellin@gmail.com>
2025-10-01 10:45:13 +0200

    Creación de la clase Receiptitem

---
+++ src/AppForSEII2526.API/Models/Receiptitem.cs
@@ -0,0 +1,9 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Receiptitem
+    {
+        public string Model { get; set; }
+        public int ReceiptId { get; set; }
+        public int RepairId { get; set; }
+    }
+}
