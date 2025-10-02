8b7e6ee220bbf5b7c49c048f098e3afd4df87cd7
Rizzzoos <151723401+Rizzzoos@users.noreply.github.com>
2025-10-02 12:21:13 +0200

    Creacion de clase Purchase (#28)

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,18 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Purchase
+    {
+        public string CustomerUserName { get; set; }
+        public string CustomerUserSurname { get; set; }
+        public string DeliveryAddress { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public double TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }
+        public enum PaymentMethodTypes
+        {
+            //Metodos de pago
+        }
+        public DateTime ReceiptDate { get; set; }
+    }
+}
