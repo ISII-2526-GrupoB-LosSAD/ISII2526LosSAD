1f9a5cc449921f790a5dffe3e8ab9fdd34df7a0d
SANDRA GARCÍA ALCOLEA <Sandra.Garcia73@alu.uclm.es>
2025-10-02 12:29:04 +0200

    Creacion de la clase PurchaseItem

---
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PurchaseItem
+    {
+        public string Description { get; set; }
+        public int DeviceId { get; set; }
+
+        public double Price { get; set; }
+        public int PurchaseId { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
