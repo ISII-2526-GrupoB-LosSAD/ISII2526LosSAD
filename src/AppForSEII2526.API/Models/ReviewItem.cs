9885ac64cf743cd8d4f3c7cae036de8cb865fcd8
Daniel Tierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-10-02 11:58:09 +0200

    Creación de la clase ReviewItem (#26)

---
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -0,0 +1,12 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class ReviewItem
+    {
+        public string Comments { get; set; }
+        public int DeviceId { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public int ReviewId { get; set; }
+    }
+}
