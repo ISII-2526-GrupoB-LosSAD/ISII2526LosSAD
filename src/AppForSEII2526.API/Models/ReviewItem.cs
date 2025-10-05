b3358e9750d3c29ee303527080ca0d466a272b38
DanielTierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-10-05 20:59:22 +0200

    Arreglada la relación ReviewItem

--- src/AppForSEII2526.API/Models/ReviewItem.cs
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -6,10 +6,12 @@
         [StringLength(100, ErrorMessage = "Comments cannot be longer than 100 characters.", MinimumLength = 4)]
         public string Comments { get; set; }
         public int DeviceId { get; set; }
+        public Device Device { get; set; }
         [Key]
         public int Id { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Minimum 1")]
         public int Rating { get; set; }
         public int ReviewId { get; set; }
+        public Review Review { get; set; }
     }
 }
