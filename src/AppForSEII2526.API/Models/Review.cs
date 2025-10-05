3da2a023d3f3c5847889c43aeb3e4e3adaf367ec
Daniel Tierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-10-05 20:37:15 +0200

    Añadir restricciones a clases Model, ReviewItem y Review, y cambiar atributos de clase Review a ApplicationUser (#46)

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -2,13 +2,16 @@
 {
     public class Review
     {
-        public int CustomerCount { get; set; }
         public int CustomerId { get; set; }
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateOfReview { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum 1")]
         public int OverallRating { get; set; }
         public int ReviewId { get; set; }
+        [StringLength(50, ErrorMessage = "Review Title cannot be longer than 50 characters.", MinimumLength = 4)]
         public string ReviewTitle { get; set; }
-        public List<ReviewItem> ReviewItems { get; set; }
+        public IList<ReviewItem> ReviewItems { get; set; }
 
     }
 }
--- src/AppForSEII2526.API/Models/ReviewItem.cs
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -1,13 +1,14 @@
 ﻿namespace AppForSEII2526.API.Models
 {
-    [PrimaryKey(nameof(DeviceId),
-nameof(ReviewId))]
+    [PrimaryKey(nameof(DeviceId),nameof(ReviewId))]
     public class ReviewItem
     {
+        [StringLength(100, ErrorMessage = "Comments cannot be longer than 100 characters.", MinimumLength = 4)]
         public string Comments { get; set; }
         public int DeviceId { get; set; }
         [Key]
         public int Id { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum 1")]
         public int Rating { get; set; }
         public int ReviewId { get; set; }
     }
