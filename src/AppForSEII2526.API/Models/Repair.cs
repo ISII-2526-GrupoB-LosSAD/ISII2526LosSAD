7603a3c5f5d9cede7d7221095dee842e1a8a5488
Antoniosqd <Antonio.Sanchez38@alu.uclm.es>
2025-10-05 20:54:25 +0200

    Añadir restricciones a clases y cambiar atributos de las clases Scale, Repeir, ReceiptItem y Receipt

--- src/AppForSEII2526.API/Models/Repair.cs
+++ src/AppForSEII2526.API/Models/Repair.cs
@@ -2,13 +2,23 @@
 {
     public class Repair
     {
+        [Range(0.0, 1000.0, ErrorMessage = "El valor debe estar entre 0 y 1000.")]
+        [Display(Name = "Float Value")]
         public float Cost { get; set; }
+
+        [StringLength(200, ErrorMessage = "Description cannot be longer than 200 characters.", MinimumLength = 4)]
         public string Description { get; set; }
+
         [Key]
         public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.", MinimumLength = 4)]
         public string Name { get; set; }
+
         public int Scaledid { get; set; }
+
         public Scale Scale { get; set; }
+
         public IList<Receiptitem> Receiptitems { get; set; }
     }
 }
