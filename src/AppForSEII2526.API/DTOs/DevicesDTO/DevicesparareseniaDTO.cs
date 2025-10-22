418e8776a221294f6e11a25ad3b78d1fe60d5672
Daniel Tierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-10-22 22:42:31 +0200

    CreacionControladorDani (#91)

---
+++ src/AppForSEII2526.API/DTOs/DevicesDTO/DevicesparareseniaDTO.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace AppForSEII2526.API.DTOS.DevicesDTO
+{
+    public class DevicesparareseniaDTO
+    {
+        public DevicesparareseniaDTO(int id, string brand, string color, string name, int year, string model)
+        {
+            Id = id;
+            Brand = brand;
+            Color = color;
+            Name = name;
+            Year = year;
+            Model = model;
+        }
+
+        public int Id { get; set; }
+
+		[StringLength(50, ErrorMessage = "Brand cannot be longer than 50 characters.", MinimumLength = 4)]
+		public string Brand { get; set; }
+
+		[StringLength(50, ErrorMessage = "Color cannot be longer than 50 characters.", MinimumLength = 4)]
+		public string Color { get; set; }
+
+		[StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.", MinimumLength = 4)]
+		public string Name { get; set; }
+
+		[Range(1, int.MaxValue, ErrorMessage = "Minimum 1")]
+		public int Year { get; set; }
+
+		[StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.", MinimumLength = 4)]
+		public string Model { get; set; }
+
+
+		
+	}
+}
