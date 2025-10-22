1047525522c90705fdb9969ae10b4faba1259f92
SANDRA GARCÍA ALCOLEA <Sandra.Garcia73@alu.uclm.es>
2025-10-22 22:43:35 +0200

    Creacion Controles Prueba Sandra

---
+++ src/AppForSEII2526.API/Controllers/DevicesController.cs
@@ -0,0 +1,54 @@
+﻿using AppForSEII2526.API.DTOs.DevicesDTO;
+using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly ILogger<DevicesController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public DevicesController(ILogger<DevicesController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if (op2 == 0)
+            {
+                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+                return BadRequest("op2 must be different from 0");
+            }
+            decimal result = decimal.Round(op1 / op2, 2);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<DevicesParaComprarDTO>), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult> GetDevicesParaComprarDTO(string? name, string? color)
+        {
+            var devices = await _context.Devices
+                .Where(d => (name == null || d.Name.Contains(name)) && (color == null || d.Color.Contains(color)))
+                .Select(d => new DevicesParaComprarDTO( d.Id, d.Name, d.priceForPurchase, d.Brand, d.Model.NameModel, d.Color ))
+                .ToListAsync();
+            return Ok(devices);
+
+        }
+
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/DevicesDTO/DevicesParaComprarDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace AppForSEII2526.API.DTOs.DevicesDTO
+{
+    public class DevicesParaComprarDTO
+    {
+        public DevicesParaComprarDTO(int id, string name, double price, string brand, string model, string color)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Brand = brand;
+            Model = model;
+            Color = color;
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "DeliveryAddress cannot be longer than 50 characters.", MinimumLength = 4)]
+        public string Name { get; set; }
+
+        [Range(0.0, 1000.0, ErrorMessage = "El valor debe estar entre 0 y 1000.")]
+        [Display(Name = "Double Value")]
+        public double Price { get; set; }
+
+        [StringLength(50, ErrorMessage = "DeliveryAddress cannot be longer than 50 characters.", MinimumLength = 4)]
+        public string Brand { get; set; }
+
+        [StringLength(50, ErrorMessage = "DeliveryAddress cannot be longer than 50 characters.", MinimumLength = 4)]
+        public string Model { get; set; }
+
+        [StringLength(50, ErrorMessage = "DeliveryAddress cannot be longer than 50 characters.", MinimumLength = 4)]
+        public string Color { get; set; }
+
+
+    }
+}
