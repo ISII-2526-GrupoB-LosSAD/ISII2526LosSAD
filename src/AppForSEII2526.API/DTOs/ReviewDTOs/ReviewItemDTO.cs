5fd15e9f21bd6c9f8a151f7758ee255dfd396d6a
DanielTierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-10-24 17:17:36 +0200

    Errores solucionados

--- src/AppForSEII2526.API/DTOs/ReviewDTOs/ReviewItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/ReviewDTOs/ReviewItemDTO.cs
@@ -5,12 +5,12 @@ namespace AppForSEII2526.API.DTOs.ReviewDTOs
     public class ReviewItemDTO
     {
         public string Name { get; set; }
-        public Model Model { get; set; }
+        public string Model { get; set; }
         public int Year { get; set; }
         public int Rating { get; set; }
         public string Comments { get; set; }
 
-        public ReviewItemDTO(string name, Model model, int year, int rating, string comments)
+        public ReviewItemDTO(string name, string model, int year, int rating, string comments)
         {
             Name = name;
             Model = model;
