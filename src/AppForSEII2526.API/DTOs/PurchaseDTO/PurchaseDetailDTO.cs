5b0c08b33952549a343683e33b91af5f0bfa9691
SANDRA GARCÍA ALCOLEA <Sandra.Garcia73@alu.uclm.es>
2025-10-24 13:31:01 +0200

    implementar metodo get de comprar dispositivo

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTO/PurchaseDetailDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace AppForSEII2526.API.DTOs.PurchaseDTO
+{
+    public class PurchaseDetailDTO
+    {
+
+        public string CostumerUserName { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public double TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }  
+
+        public IList<PurchaseItemDTO> PurchaseItems { get; set; }
+
+        public PurchaseDetailDTO(string costumerUserName, string deliveryAddress, DateTime purchaseDate, double totalPrice, int totalQuantity, IList<PurchaseItemDTO> PurchaseItems)
+        {
+            this.CostumerUserName = costumerUserName;
+            this.DeliveryAddress = deliveryAddress;
+            this.PurchaseDate = purchaseDate;
+            this.TotalPrice = totalPrice;
+            this.TotalQuantity = totalQuantity;
+            this.PurchaseItems = PurchaseItems;
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTO/PurchaseItemDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace AppForSEII2526.API.DTOs.PurchaseDTO
+{
+    public class PurchaseItemDTO
+    {
+
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public Model Model { get; set; }
+
+        public PurchaseItemDTO(string description, double price, int quantity, string brand, string color, Model model)
+        {
+            this.Description = description;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Brand = brand;
+            this.Color = color;
+            this.Model = model;
+        }
+    }
+}
