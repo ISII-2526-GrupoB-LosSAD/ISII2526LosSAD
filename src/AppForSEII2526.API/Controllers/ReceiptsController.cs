af6ee09bd7ba26d08836660f637190b19e1de7de
Antonio Sánchez-Quintanar Díaz-Hellín <Antonio.Sanchez38@alu.uclm.es>
2025-10-30 18:59:41 +0100

    Implementar método post de crear el caso de uso de contratar una reparación (#113)

--- src/AppForSEII2526.API/Controllers/ReceiptsController.cs
+++ src/AppForSEII2526.API/Controllers/ReceiptsController.cs
@@ -1,4 +1,5 @@
 ﻿using AppForSEII2526.API.DTOs.ReceiptDTOs;
+using AppForSEII2526.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -52,6 +53,85 @@ namespace AppForSEII2526.API.Controllers
                 return NotFound();
             }
             return Ok(receipt);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ReceiptDetailDTO), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        public async Task<ActionResult>CreateRepair(ReceiptForCreateDTO receiptForCreate)
+        {
+            if (receiptForCreate.receiptItems.Count == 0)
+                ModelState.AddModelError("RentalItems", "Error! You must include at least one movie to be rented");
+
+            var user = _context.Users.FirstOrDefault(au => au.CustomerUserName == receiptForCreate.CustomerUserName);
+            if (user == null)
+                ModelState.AddModelError("RentalApplicationUser", "Error! UserName is not registered");
+
+            // Si hay errores
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+            var repairNames = receiptForCreate.receiptItems.Select(ri => ri.Name).ToList();
+            var repair= _context.Repairs.Include(r=>r.Receiptitems)
+                .Include(r=>r.Scale)
+                .Where(r => repairNames.Contains(r.Name))
+            //creamos un tipo anónimo para traer solo los datos necesarios
+                .ToList();
+            Receipt receipt = new Receipt(receiptForCreate.DeliveryAddress,DateTime.Now,receiptForCreate.receiptPaymentMethodTypes, new List<Receiptitem>(), user);
+
+            // Crear los receipt items y añadirlos al receipt
+            foreach (var item in receiptForCreate.receiptItems)
+            {
+                var repairItem = repair.FirstOrDefault(r => r.Name == item.Name);
+                if (repairItem == null)
+                {
+                    ModelState.AddModelError("ReceiptItems", $"The repair '{item.Name}' does not exist.");
+                }
+                else
+                {
+                    receipt.Receiptitems.Add(new Receiptitem(item.Model,receipt,repairItem));
+
+                    receipt.TotalPrice = receipt.TotalPrice + item.Cost;
+                }
+            }
+            if (ModelState.ErrorCount > 0)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+            
+            _context.Add(receipt);
+            
+            try
+            {
+                
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                ModelState.AddModelError("ReceiptCreation", "There was an error while saving your rental, plese, try again later");
+                return Conflict("Error" + ex.Message);
+            }
+            // Devolver el receipt creado
+            var receiptDetail = new ReceiptDetailDTO(
+                user.CustomerUserName,
+                user.CustomerUserSurname,
+                receipt.DeliveryAddress,
+                receipt.TotalPrice,
+                receipt.ReceiptDate,
+                receipt.Receiptitems.Select(ri => new ReceiptitemDTO(
+                    ri.Repair.Name,
+                    ri.Repair.Scale.Name,
+                    ri.Repair.Cost,
+                    ri.Model
+                )).ToList()
+            );
+
+            return CreatedAtAction("GetReceipt", new { id = receipt.Id }, receiptDetail);
+
+
         }
     }
 }
