2bae6b08090f182061f376622ad52f657e4ff89b
DanielTierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-11-11 19:28:02 +0100

    Practica 3 distribuidos

--- src/AppForSEII2526.API/Program.cs
+++ src/AppForSEII2526.API/Program.cs
@@ -1,8 +1,10 @@
+using AppForSEII2526.API.Logging;
 using Microsoft.Data.Sqlite;
 using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Logging.AddRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
+//”RabbitMQ” coincide con el nombre del bloque de propiedades en appsettings.json
 // Add services to the container.
 
 builder.Services.AddControllers()
