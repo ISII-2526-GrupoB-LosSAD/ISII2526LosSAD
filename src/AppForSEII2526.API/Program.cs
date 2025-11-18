9f4e9175c4d1b3f5c967e1bc863e75b896a39430
DanielTierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-11-18 19:25:00 +0100

    Código comentado

--- src/AppForSEII2526.API/Program.cs
+++ src/AppForSEII2526.API/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Data.Sqlite;
 using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.AddRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
+//builder.Logging.AddRabbitMQ(builder.Configuration.GetSection("RabbitMQ"));
 //”RabbitMQ” coincide con el nombre del bloque de propiedades en appsettings.json
 // Add services to the container.
 
