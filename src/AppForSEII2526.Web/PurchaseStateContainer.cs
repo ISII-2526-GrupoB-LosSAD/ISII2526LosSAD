93a67ab5b20e0e184a23e5f8a17786c3e98e8d01
SANDRA GARCÍA ALCOLEA <Sandra.Garcia73@alu.uclm.es>
2025-12-09 18:42:27 +0100

    Desarollar ui para realizar compra

---
+++ src/AppForSEII2526.Web/PurchaseStateContainer.cs
@@ -0,0 +1,71 @@
+﻿using AppForSEII2526.Web.API;
+namespace AppForSEII2526.Web
+{
+    public class PurchaseStateContainer
+    {
+
+
+        //we create an instance of Purchase when an instance of RentalStateContainer is created
+        public PurchaseForCreateDTO Purchase { get; private set; } = new PurchaseForCreateDTO()
+        {
+            PurchaseItems = new List<PurchaseItemDTO>()
+        };
+
+        //we compute the TotalPrice of the movies we have selected for renting them
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Convert.ToDecimal(Purchase.PurchaseItems.Sum(ri => ri.Price * ri.Quantity));
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+
+        public void AddDeviceToPurchase(DevicesParaComprarDTO device)
+        {
+            //before adding a movie we checked whether it has been already added
+            if (!Purchase.PurchaseItems.Any(ri => ri.DeviceID == device.Id))
+                //we add it if it is not in the list
+                Purchase.PurchaseItems.Add(new PurchaseItemDTO()
+                {
+                    DeviceID = device.Id,
+                    Description = device.Name,
+                    Price = device.Price,
+                    Brand = device.Brand,
+                    Model = device.Model,
+                    Color = device.Color
+                }
+            );
+
+        }
+
+        //to delete movies from the list of selected movies
+        public void RemovePurchaseItemToRent(PurchaseItemDTO item)
+        {
+            Purchase.PurchaseItems.Remove(item);
+
+        }
+
+        //we eliminate all the movies from the list
+        public void ClearRentingCart()
+        {
+            Purchase.PurchaseItems.Clear();
+
+        }
+
+        //we have already finished the process of renting, thus, we create a new Rental 
+        public void PurchaseProcessed()
+        {
+            //we have finished the rental process so we create a new object without data
+            Purchase = new PurchaseForCreateDTO()
+            {
+                PurchaseItems = new List<PurchaseItemDTO>()
+            };
+        }
+    }
+}
