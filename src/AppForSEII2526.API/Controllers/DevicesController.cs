a204af79c467f9adb040c945fdc8ae10e3f9980e
DanielTierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-12-12 13:23:43 +0100

    error corregido

--- src/AppForSEII2526.API/Controllers/DevicesController.cs
+++ src/AppForSEII2526.API/Controllers/DevicesController.cs
@@ -23,7 +23,7 @@ namespace AppForSEII2526.API.Controllers
         {
             _logger = logger;
             _context = context;
-            _logger.LogInformation("Soy el constructor del dispositivo");
+            //_logger.LogInformation("Soy el constructor del dispositivo");
         }
 
         [HttpGet]
