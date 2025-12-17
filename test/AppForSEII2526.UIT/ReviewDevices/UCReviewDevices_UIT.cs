bc6df156631ce981847a58cdf0b26770068a6e13
DanielTierraseca <Daniel.Tierraseca@alu.uclm.es>
2025-12-17 17:30:25 +0100

    Pruebas del create

--- test/AppForSEII2526.UIT/ReviewDevices/UCReviewDevices_UIT.cs
+++ test/AppForSEII2526.UIT/ReviewDevices/UCReviewDevices_UIT.cs
@@ -2,9 +2,11 @@
 using OpenQA.Selenium.DevTools.V141.Security;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AppForSEII2526.UIT.ReviewDevices
 {
@@ -101,5 +103,86 @@ namespace AppForSEII2526.UIT.ReviewDevices
             //Assert            
             Assert.True(selectDevices.CheckReviewDeviceDisabled(), "You must select at least one device");
         }
+
+
+        [Theory]
+        [InlineData("Elena", "Spain",  "", "Reseña para", 5, "The ReviewTitle field is required.")]
+        [InlineData("Elena", "",  "Perfecto rendimiento", "Reseña para", 5, "The Country field is required.")]
+        [InlineData("Elena", "Spain", "Perfecto rendimiento", "Reseña para", null, "")]
+        [InlineData("Elena", "Spain", "Perfecto rendimiento", "", 5, "")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_6_7_8_9_testingErrorsMandatorydata(string username, string country, string reviretitle,string comentario, int rating,
+           string expectedMessageError)
+        {
+            //Arrange
+
+            var createreview = new CreateReviewPO(_driver, _output);
+
+            //Act
+            InitialStepsForReviewDevice_UIT();
+
+            selectDevices.SelectDevices(new List<string> { deviceId1.ToString() });
+            selectDevices.ReviewDevices();
+            createreview.FillInReviewInfo(reviretitle, username, country);
+            Thread.Sleep(1000);
+            
+            createreview.PressReviewYourDevices();
+
+            //Assert
+            //the expected error is shown in the view
+            Assert.True(createreview.CheckValidationError(expectedMessageError), $"Expected error: {expectedMessageError}");
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_10_ModifyRentalItems()
+        {
+            //Arrange
+
+            var createreview = new CreateReviewPO(_driver, _output);
+
+            //Act
+            InitialStepsForReviewDevice_UIT();
+
+            
+            selectDevices.SelectDevices(new List<string> { deviceId1.ToString()});
+            selectDevices.ReviewDevices();
+            Thread.Sleep(1000);
+            createreview.PressModifyMovies();
+            
+
+            //Assert
+            //the list of movies must change
+            Assert.True(selectDevices.CheckShoppingCart(deviceName1));
+        }
+        [Theory]
+        [InlineData("Elena", "Spain", "Perfecto rendimiento", "Reseña para", -5, "")]
+        [InlineData("Elena", "Spain", "Perfecto rendimiento", "No sirve", 5, "")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_11_12_testingErrorsvalidationdata(string username, string country, string reviretitle, string comentario, int rating,
+          string expectedMessageError)
+        {
+            //Arrange
+
+            var createreview = new CreateReviewPO(_driver, _output);
+
+            //Act
+            InitialStepsForReviewDevice_UIT();
+
+            selectDevices.SelectDevices(new List<string> { deviceId1.ToString() });
+            selectDevices.ReviewDevices();
+            createreview.FillInReviewInfo(reviretitle, username, country);
+            createreview.AddDeviceReviewComent(deviceId1, comentario);
+            Thread.Sleep(1000);
+            createreview.AddDeviceReviewRating(deviceId1, rating);
+            Thread.Sleep(1000);
+
+            createreview.PressReviewYourDevices();
+
+            //Assert
+            //the expected error is shown in the view
+            Assert.True(createreview.CheckValidationError(expectedMessageError), $"Expected error: {expectedMessageError}");
+        }
+
     }
 }
